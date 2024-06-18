6aed1b74d46b0164da476395a8d2849c6d0a0fd2
Javier Fray <fraydeveloper@cacaowebstudio.com>
2024-06-17 21:43:33 -0500

    add alt attribute to logo

--- src/Books/components/Header.tsx
+++ src/Books/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function BookHeader() {
     return (
         <header className="flex items-center p-5 md:p-8 pb-2 border-x-0 border-t-0 border-b dark:border-b-stone-300">
-            <img src="/javier_logo.jpeg" className="w-20 rounded-md mr-2" loading="lazy"></img>
+            <img src="/javier_logo.jpeg" className="w-20 rounded-md mr-2" alt="MonkeyDeveloper (Javier Fray) logo" loading="lazy"></img>
             <h1 className="text-2xl font-bold md:text-4xl">
                 Biblioteca de libros
             </h1>
